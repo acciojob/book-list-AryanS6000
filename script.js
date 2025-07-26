1c7e04f4587f77864656d09e2ad61830b6126f0f
AryanS6000 <163299006+AryanS6000@users.noreply.github.com>
2025-07-26 18:59:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+ const submitBtn = document.getElementById("submit");
+
+ submitBtn.addEventListner("click",function(){
+	 const title = document.getElementById("title").value.trim();
+	 const author = document.getElementById("author").value.trim();
+	 const isbn = document.getElementById("isbn").value.trim();
+
+	 const tablebody = document.getElementById("book-list");
+	 const row = document.createElement("tr");
+
+	 row.innerHTML = `
+	     <td>${title}</td>
+		 <td>${author}</td>
+         <td>${isbn}</td>
+		 <td><button class = "delete">Clear</button></td>
+		 `;
+	 tablebody.appendChild(row);
+
+	 document.getElementById("title").value ="";
+	 document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";  
+ });
+document.getElementById("book-list").addEventListener("click", function (event) {
+  // Agar clicked element ka class "delete" hai
+  if (event.target.classList.contains("delete")) {
+    // Us button ke parent row ko hata do
+    const rowToDelete = event.target.parentElement.parentElement;
+    rowToDelete.remove();
+  }
+});
